be76ebd39ec76428521a5bfda6036f23adabdbf6
Miguel Pérez García <miguelpg_95@hotmail.com>
2017-11-02 19:44:02 -0600

    correcting not waiting for threads

--- thread/hellow.c
+++ thread/hellow.c
@@ -5,11 +5,14 @@
 
 #define NUM_THREADS     5
 
-void *PrintHello(void *threadid)
+unsigned int total;
+
+void *add100(void *threadid)
 {
    long tid;
    tid = (long)threadid;
-   printf("Hello World! It's me, thread #%ld!\n", tid);
+   printf("Adding 100 #%ld!\n", tid);
+   total += 100;
    pthread_exit(NULL);
 }
 
@@ -18,14 +21,23 @@ int main (int argc, char *argv[])
    pthread_t threads[NUM_THREADS];
    int rc;
    long t;
+
+   printf("Initial total = %d \n", total);
+
    for(t=0; t<NUM_THREADS; t++){
       printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      rc = pthread_create(&threads[t], NULL, add100, (void *)t);
       if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
          exit(-1);
       }
    }
+
+   for(t = 0; t < NUM_THREADS; t++){
+       pthread_join(threads[t], NULL);
+   }
+
+   printf("Las total = %d\n", total);
 
    /* Last thing that main() should do */
    pthread_exit(NULL);
